e010302dfdb3a7df900e9c0573668a479935d614
rofviv <royvillarroel94@gmail.com>
2024-11-08 16:49:32 -0400

    update scripts

--- scripts/nextion_data.py
+++ scripts/nextion_data.py
@@ -3,12 +3,11 @@ import pyvisa as visa
 import time
 import requests
 
-# TODO: SET IP STATIC ARDUINO
-base_ip_relay_env = os.getenv('BASE_IP_RELAY', '0.0.0.0')
+project_root = os.getenv('PROJECT_ROOT', '')
 base_url_patio_env = os.getenv('BASE_URL_PATIO', 'https://www.patio-driver.patiodelivery2.com')
 token_patio_env = os.getenv('TOKEN_PATIO', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDQsInVzZXJuYW1lIjoic2NAdGVzdC5jb20iLCJpYXQiOjE3MjE3NjY5NjgsImV4cCI6MTc1MzMwMjk2OH0.UrIetzHOSBcTlMdeOD_TRyj4eLIHMdzx-SxZXSfwQM4')
 
-baseUrlRover = f"http://{base_ip_relay_env}"
+baseUrlRover = "http://0.0.0.0"
 baseUrlPatio = base_url_patio_env
 # TODO: GET TOKEN ROVER APP
 tokenPatio = token_patio_env
@@ -16,6 +15,15 @@ tokenPatio = token_patio_env
 print("Starting nextion script...")
 ports = visa.ResourceManager()
 print(ports.list_resources())
+
+def read_ip_relay():
+  global baseUrlRover
+  try:
+    with open(f"{project_root}\ip_relay.txt", "r") as file:
+      baseUrlRover = file.read().strip()
+      print(f"Base URL Rover: {baseUrlRover}")
+  except Exception as e:
+      print(f"Error al leer el archivo ip_relay.txt: {e}")
 
 # TODO: SET INPUT CORRECT (CHECK CONSOLE)
 while True:
@@ -28,17 +36,39 @@ while True:
     print(f"Error al conectar al puerto serie:. Reintentando en 5 segundos...")
     time.sleep(5)
 
+def isOpenDoorOne() -> bool:
+  # {
+  #   "status": "Success",
+  #   "relay1": 1,
+  #   "relay2": 0,
+  #   "relay3": 0,
+  #   "relay4": 0,
+  #   "relay5": 0,
+  #  "relay6": 0,
+  #  "relay7": 0,
+  #  "relay8": 1,
+  #  "ssid": "OMEN-PC"
+  # }
+  response = requests.get(baseUrlRover)
+  return response.json()["relay8"] == 1
+
+def isOpenDoorTwo() -> bool:
+  response = requests.get(baseUrlRover)
+  return response.json()["relay5"] == 1
+
 def openDoorOne():
-  # requests.get(baseUrlRover+'/first-lock?value=1')
-  requests.get(baseUrlRover+'/relay8')
+  if not isOpenDoorOne():
+    requests.get(baseUrlRover+'/relay8')
+
   serialPort.write_raw(b'textComplete.txt="Opening door number one"')
   serialPort.write_raw(b'\xff\xff\xff')
   serialPort.write_raw(b'buttonComplete.txt="Close 1"')
   serialPort.write_raw(b'\xff\xff\xff')
   
 def openDoorTwo():
-  # requests.get(baseUrlRover+'/second-lock?value=1')
-  requests.get(baseUrlRover+'/relay5')
+  if not isOpenDoorTwo():
+    requests.get(baseUrlRover+'/relay5')
+
   serialPort.write_raw(b'textComplete.txt="Door number two open, open the door"')
   serialPort.write_raw(b'\xff\xff\xff')
   serialPort.write_raw(b'buttonComplete.txt="Close 2"')
@@ -87,6 +117,8 @@ def verifyPassword(password):
     time.sleep(2)
     findOrderByPassword(password)
 
+read_ip_relay()
+
 while(True):
   if(serialPort.bytes_in_buffer>0):
     data = serialPort.read_bytes(serialPort.bytes_in_buffer).decode('utf-8', errors='ignore')
@@ -95,9 +127,9 @@ while(True):
       password = data.replace('enter', '')
       verifyPassword(password)
     elif data == "Close 1":
-      # requests.get(baseUrlRover+'/first-lock?value=0')
-      requests.get(baseUrlRover+'/relay8')
+      if isOpenDoorOne():
+        requests.get(baseUrlRover+'/relay8')
     elif data == "Close 2":
-      # requests.get(baseUrlRover+'/second-lock?value=0')
-      requests.get(baseUrlRover+'/relay5')
+      if isOpenDoorTwo():
+        requests.get(baseUrlRover+'/relay5')
   time.sleep(2)
